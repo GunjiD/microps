1bcbc06d6ca6461429aa88172106fea192959feb
com_gunzi <nolispnolife@gmail.com>
2023-08-11 00:35:15 +0900

    step4 プロトコルの管理

--- net.c
+++ net.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "platform.h"
 
 #include "util.h"
 #include "net.h"
+#include "ip.h"
 
+struct net_protocol {
+  struct net_protocol *next; 
+  uint16_t type;
+  struct queue_head queue; /* input queue */
+  void (*handler)(const uint8_t *data, size_t len, struct net_device *dev);
+};
+
+struct net_protocol_queue_entry {
+  struct net_device *dev;
+  size_t len;
+  uint8_t data[];
+};
 
 /* NOTE: if you want to add/delete the entries after net_run(),  you need to protect these lists with a mutex */
 static struct net_device *devices; // デバイスリスト(リストの先頭を指すポインタ)
-
+static struct net_protocol *protocols; // 登録されているプロトコルのリスト
 
 // デバイス構造体のサイズのメモリを確保
 // ・memory_alloc() で確保したメモリ領域は0で初期化されている
@@ -114,10 +128,69 @@ net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, si
   return 0;
 }
 
+/* NOTE: must not be call after net_run() */
+int
+net_protocol_register(uint16_t type, void (*handler)(const uint8_t *, size_t, struct net_device *))
+{
+  struct net_protocol *proto;
+
+  // 重複登録の確認
+  for (proto = protocols; proto; proto = proto->next) {
+    if (type == proto->type) {
+      errorf("already registered, type=0x%04x", type);
+      return -1;
+    }
+  }
+
+  // プロトコル構造体のメモリを確保
+  proto = memory_alloc(sizeof(*proto));
+  if (!proto) {
+    errorf("memory_alloc() failure");
+    return -1;
+  }
+
+  // プロトコル種別と入力関数を設定
+  proto->type = type;
+  proto->handler = handler;
+
+  // プロトコルリストの先頭に追加
+  proto->next = protocols;
+  protocols = proto;
+
+  infof("registered, type=0x%04x", type);
+  
+  return 0;
+}
+
 int net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
-  debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
-  debugdump(data, len);
+  struct net_protocol *proto;
+  struct net_protocol_queue_entry *entry;
+
+  for(proto = protocols; proto; proto = proto->next) {
+    if (proto->type == type) {
+      // 新しいエントリのメモリを確保
+      entry = memory_alloc(sizeof(*entry) + len);
+      if (!entry) {
+	errorf("memory_alloc() failure");
+	return -1;
+      }
+
+      // 新しいエントリへメタデータの設定と受信データのコピー
+      entry->len = len;
+      entry->dev = dev;
+      memcpy(entry->data, data, len);
+
+      // キューに新しいエントリを挿入
+      queue_push(&proto->queue, entry);
+      
+      debugf("queue pushed (num:%u), dev=%s, type=0x%04x, len=%zu",
+	     proto->queue.num, dev->name, type, len);
+      debugdump(data, len);
+      return 0;
+    }
+  }
+  /* unsupported protocols */
   return 0;
 }
 
@@ -160,6 +233,11 @@ net_init(void)
 {
   if (intr_init() == -1) {
     errorf("intr_init() failure");
+    return -1;
+  }
+
+  if (ip_init() == -1) {
+    errorf("ip_init() failure");
     return -1;
   }
   
